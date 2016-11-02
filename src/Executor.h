945be98b2e695e5d5e9f953672101b37fd074232
Kuzame <aharm002@ucr.edu>
2016-11-01 23:26:21 -0700

    ALMOST DONEgit add main.cpp...

--- src/Executor.h
+++ src/Executor.h
@@ -2,8 +2,8 @@
 #define EXECUTOR_H
 
 #include <sys/types.h>
-//#include <sys/wait.h>
-//#include <unistd.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <signal.h>
 
 class Executor : public Base {
@@ -13,11 +13,12 @@ private:
 	int cases;
 	
 	//fork handlers
-	//pid_t child, c;
-	int cstatus, getPID;
+	int pip[2];
+	pid_t child, c; //c is needed for testing, unnecessary for actual codes
+	int cstatus, getPID; //getPID is also needed for testing to determine's the parent's ID
 	Tokenizer * tokenizer;
 	vector <const char*> commands;
-
+	
 	void executor (unsigned i) {
 		int size = (int) tokenizer->getVector().at(i)->getSubTokensVect().size();
 		char * args [size+1];
@@ -29,53 +30,59 @@ private:
 		
 		args[size] = NULL; // putting NULL at the end of arguments, so execvp knows when to stop reading
 		
-		// Will yield ERROR if we put: lsa || echoHello && echo hi
-		// Something's wrong because the child is executing its task
-		// Solution: Maybe have something to do with KILL the child's process
 		if ((isFirstTime())== 0) {					// if it's 0
-			cout<< "Child "<< getpid()<< " calls this: "; //for debugging
+			close(pip[0]);
+			//cout<< "Child "<< getpid()<< " calls this: "; //for debugging
 			previousState=execvp(args[0], args);
 			
 			//it should exit by the point above, otherwise it fails (below)
 			previousState=false;
-			cerr<< "Failed to do execvp\n";
+			cerr<< "Failed to do execvp!\n";
+			close(pip[1]);
 		}
 		else if (child == (pid_t)(-1)) {
 			cerr << "Failed to do fork\n";
 		}
 		else {									// if it's positive
+			close(pip[1]);
 			do {
 				c = wait(&cstatus);
 			}
 			while (c== -1);
-			cout<<"--for debugging-- Parent: child " << c << " exits. getpid is " << getpid()<<endl; // for debugging
-			cout<<"--killing parent " << getpid() << "!"<<endl; // for debugging
+			//cout<<"--for debugging-- Parent: child " << c << " exits. getpid is " << getpid()<<endl; // for debugging
+			//cout<<"--killing parent " << getpid() << "!"<<endl; // for debugging
 			//cout<<" kill status: "<< kill(getpid(), SIGKILL)<<endl;
+			close(pip[0]);
 			
 		}
 		
 		
 		delete * args; //prevent memory leak
-    }
+	}
 	
-	int isFirstTime() { // so we only will call fork ONCE when we're about to perform execvp, to prevent child reproduction
-//		if (firstTime) {
-//			getPID= getpid();
-//			cout<<"isFirstTime, getpid: "<<getPID<<endl;
-//			//kill(getPID, SIGTERM);
-//			firstTime=false;
-//		}
-		getPID=getpid();
-		child = fork();
-		cout<< "Now child is: " << getpid() << ", parent is: "<<getPID<<endl;
-		//kill(getpid(), SIGCONT);
-		return child;
+	int isFirstTime() { // so we only will call fork ONCE set the pipe for the parent, to prevent child "reproduction"`
+		if (firstTime) {
+			pipe(pip);
+			getPID= getpid();
+			child = fork();
+			//cout<<"isFirstTime, getpid: "<<getPID<<endl;
+			//kill(getPID, SIGTERM);
+			firstTime=false;
+			return child;
+		}
+		else {
+			getPID=getpid();
+			child = fork();
+			//cout<< "Now child is: " << getpid() << ", parent is: "<<getPID<<endl; //for debugging
+			//kill(getpid(), SIGCONT);
+			return child;
+		}
 	}
 	
 	void vectorSetter(unsigned i) {
-//		for (unsigned j = 0 ; j<tokenizer->getVector().at(i)->getSubTokensVect().size(); j++) {
-//			commands.push_back(tokenizer->getVector().at(i)->getSubTokensVect().at(j));
-//		}
+		//		for (unsigned j = 0 ; j<tokenizer->getVector().at(i)->getSubTokensVect().size(); j++) {
+		//			commands.push_back(tokenizer->getVector().at(i)->getSubTokensVect().at(j));
+		//		}
 	}
 	
 	int operatorHandling (unsigned i) {
@@ -91,42 +98,53 @@ private:
 			//cout << "is ;!";
 			return 2; // 2 is for Semicolon
 		}
-		else return 3; // 3 is for normal operation
+		else if (tokenizer->getVector().at(i)->getSubTokensVect().at(0) == "exit") {
+			//cout << "is ;!";
+			return 3; // 3 is for exit
+		}
+		else return 4; // 3 is for normal operation
 	}
-    
+	
 public:
-    Executor (Tokenizer * tokenizer) {
-        this->tokenizer = tokenizer;
-        this->previousState=true;
+	Executor () {
+//		this->tokenizer = NULL;
+	}
+	
+	Executor (Tokenizer * tokenizer) {
+		this->tokenizer = tokenizer;
+		this->previousState=true;
 		this->firstTime=true;
 		this->execute();
-    }
-    
-    void setTokenizer (Tokenizer * tokenizer) {
-        this->tokenizer = tokenizer;
-    }
-    
-    void print () {
-        for (unsigned i = 0; i < tokenizer->getVector().size(); i++)
-        {
-            cout <<  tokenizer->getVector().at(i)->getValue() << "\n";
-        }
-    }
-//    
-//    void printCommands() {
-//        whiteSpaceHandler(0);
-//        for (unsigned i = 0; i < command.size(); i++)
-//        {
-//            cout << "."<< command.at(i) << ".\n";
-//        }
-//    }
-    
+	}
+	
+	void setTokenizer (Tokenizer * tokenizer) {
+//		delete this->tokenizer;
+		this->tokenizer = tokenizer;
+		this->previousState=true;
+		this->firstTime=true;
+	}
+	
+//	void print () {
+//		for (unsigned i = 0; i < tokenizer->getVector().size(); i++)
+//		{
+//			cout <<  tokenizer->getVector().at(i)->getValue() << "\n";
+//		}
+//	}
+	//
+	//    void printCommands() {
+	//        whiteSpaceHandler(0);
+	//        for (unsigned i = 0; i < command.size(); i++)
+	//        {
+	//            cout << "."<< command.at(i) << ".\n";
+	//        }
+	//    }
+	
 	bool execute() {
 		// cout<< "tokenizer's size is " << tokenizer->getVector().size()<<endl; // for debugging
 		for (unsigned i = 0; i < tokenizer->getVector().size(); i++)
 		{
 			cases = operatorHandling(i);
-			cout << "-----i="<<i<<" & "<<previousState << " < prevState-----"<< getpid()<<"\n"; //for debugging
+//			cout << "-----i="<<i<<" & "<<previousState << " < prevState-----"<< getpid()<<"\n"; //for debugging
 			switch (cases) {
 				case 0: { // case ||
 					if (previousState) {
@@ -143,22 +161,28 @@ public:
 				}; break;
 					
 				case 2: { // case ;
+					previousState=true;
 					// do nothing for this token, aka move on without any condition
 				}; break;
 					
-				case 3: { // case normal operations
+				case 3: { // case exit
+					return false;
+				}
+					
+				case 4: { // case normal operations
 					if (previousState) {
 						vectorSetter(i);
 						executor(i);
 					}
 				}; break;
+				
 				default: cout<< "Something is wrong#@!\n"; break;
 			}
 		}
 		
-		cout<< "###########  EXIT THE EXECUTE  #############\n";	//for debugging
-        return true;
-    }
+		//cout<< "###########  EXIT THE EXECUTE  #############\n";	//for debugging
+		return true;
+	}
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,48 +5,56 @@
 #include "Base.h"
 #include "Token.h"
 #include "Tokenizer.h"
-//#include "Executor.h"
-//#include "Executor.h"
+#include "Executor.h"
+#include <pwd.h>
 
 using namespace std;
 
 int main(){
-	string input;
-	bool exitState;
-	Tokenizer * tokenizer = new Tokenizer();
-	//Executor * execute = new Executor();
-
-	cout << "$ ";
+	//extra credit part, components needed for getting user's username and local machine's name
+	string userLogin, result;
+	int charSize=100;
+	char localhost[charSize];
+	int validHost = gethostname(localhost, charSize);
+	struct passwd *pwd = getpwuid(getuid());
 	
+	if (validHost!= -1 && pwd!=NULL) {
+		userLogin = pwd->pw_name;
+		result = userLogin + "@" + localhost + "$ ";
+	}
+	else {
+		cerr<< "Failed to obtain user's username and local machine's name\n";
+		result = "$ ";
+	}
+	
+	//Basics needed to perform rshell
+	string input;
+	bool exitState=true;
+	Executor * execute = new Executor();
 	
 	do {
+		cout << result; //printing the extra credit here
 		//gets next command
 		getline(cin, input);
 		
-		//sets value inside toknizer class
-		tokenizer->setVal(input);
-		//executes the tokenizing operation
-		tokenizer->execute();
-		vector<Token *> temp = tokenizer->getVector();
-
 		
-// 		//sets value inside the execute class
-// 		execute->setVal(tokenizer);
-// 		//executes the command operations inside execute
-// 		exitState = execute->execute();
-		if (input == "exit")
-		{
-			exitState = false;
-		}
-		else
-		{
-			exitState = true;
-		}
-
+		Tokenizer * tokenizer = new Tokenizer(input);
+//		//sets value inside toknizer class
+//		tokenizer->setVal(input);
+//		//executes the tokenizing operation
+//		tokenizer->execute();
+//		vector<Token *> temp = tokenizer->getVector();
+		
+		
+ 		//sets value inside the execute class
+ 		execute->setTokenizer(tokenizer);
+ 		//executes the command operations inside execute
+ 		exitState = execute->execute();
+		delete tokenizer;
+		input = '\0';
 	}
 	while (exitState);
 
-	delete tokenizer;
 	//delete execute;
 	
 	//execute->execute();
