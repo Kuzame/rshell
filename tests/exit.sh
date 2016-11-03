5f103bf6ab59a07625d88556f62036909d2fcf79
Kuzame <aharm002@ucr.edu>
2016-11-02 19:42:28 -0700

    fix errors on executor and +other tests file

--- tests/exit.sh
+++ tests/exit.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-echo 'ls -l; exit' | ../bin/rshell
-echo 'ls -l && exit' | ../bin/rshell
+echo "
+##### BEGIN EXIT.H TEST #####
+----- Test exit.h 1 ------"
+echo -e 'exit' | ../bin/rshell
+echo "
+----- Test exit.h 2 ------"
+echo -e "ls -l; exit" | ../bin/rshell
+echo "
+----- Test exit.h 3 ------"
+echo -e ' echo " Heyyy!.. " && syntaxerror ; exit' | ../bin/rshell
+echo "
+----- Test exit.h 4 ------"
+echo -e 'echo exit ; lol && asdfasdf' | ../bin/rshell
+echo "
+----- Test exit.h 5 ------"
+echo -e 'asdfasdf   &&   wuuuttt  ;  exit  ||  gimme  octocatzz ' | ../bin/rshell
+echo "###### END  EXIT.H TEST #####
+"
